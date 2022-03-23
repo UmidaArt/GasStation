2f0108d4066ca465ed81c95386923282d6a47542
Erjan Kaldybaev <era.kaldybaev@gmail.com>
2022-03-23 16:48:19 +0100

    edit script file

--- script.js
+++ script.js
@@ -10,30 +10,37 @@ const btn = document.querySelector('.btn')
 
 const petrols = ['Ai95', 'AI98', 'AI100']
 let petrolType = 'AI95'
-let oneLiterPrice = 67
+let oneLiterPrice = 0
 let vat = 5
 
 
 gasStations.forEach((petrolStation,idx) => {
+    if (petrolStation.classList.contains('active')) {
+        clickPetrol()
+    }
     petrolStation.addEventListener('click', () => {
         gasStations.forEach(item => item.classList.remove('active'))
         petrolStation.classList.add('active')
+        clickPetrol()
         petrolType = petrols[idx]
         oneLiterPrice = petrolStationInput[idx].value
     })
 })
 
 btn.addEventListener('click', () => {
-    if (totalLitersInput.value.length === 0) {
-        alert('Введите количество литров')
-    } else if (oneLiterPrice.length === 0) {
+    if (oneLiterPrice.length === 0) {
         alert('Выведите цену топливо')
+    } else if (totalLitersInput.value.length === 0) {
+        alert('Введите количество литров')
     } else {
-        petrolTypeBill.textContent = petrolType
-        priceOneLiterBill.textContent = oneLiterPrice
-        totalLiterBill.textContent = totalLitersInput.value
-        vatBill.textContent = `${vat}%`
-        totalPriceBill.textContent = (oneLiterPrice * totalLitersInput.value) * 1.05.toFixed(2)
+       clickPetrol()
     }
 
 })
+function clickPetrol () {
+    petrolTypeBill.textContent = petrolType
+    priceOneLiterBill.textContent = oneLiterPrice
+    totalLiterBill.textContent = totalLitersInput.value
+    vatBill.textContent = `${vat}%`
+    totalPriceBill.textContent = (oneLiterPrice * totalLitersInput.value) * 1.05.toFixed(2)
+}
